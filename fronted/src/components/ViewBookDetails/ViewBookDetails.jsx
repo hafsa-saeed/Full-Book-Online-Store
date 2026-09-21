89e8b44d80126c79f261c656a2b04153e52020bc
Hafsa Saeed <hafsasaeed1074@gmail.com>
2026-09-21 08:39:09 -0700

    Change API endpoint to production URL

--- fronted/src/components/ViewBookDetails/ViewBookDetails.jsx
+++ fronted/src/components/ViewBookDetails/ViewBookDetails.jsx
@@ -18,7 +18,7 @@ const ViewBookDetails = () => {
   useEffect(() => {
     const fetch = async () => {
       const response = await axios.get(
-        `http://localhost:1000/api/v1/get-book-by-id/${id}`
+        `https://full-book-online-store-nqqu-hyax2vqy0.vercel.app/api/v1/get-book-by-id/${id}`
       );
       setData(response.data.data);
     };
