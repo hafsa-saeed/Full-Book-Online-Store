329d26bf88d9c6fea076a7442f971893a603c8ea
Hafsa Saeed <hafsasaeed1074@gmail.com>
2026-09-21 09:24:50 -0700

    Update AddBook.jsx

--- fronted/src/pages/AddBook.jsx
+++ fronted/src/pages/AddBook.jsx
@@ -34,7 +34,7 @@ const AddBook = () => {
         alert("All fields are required");
       } else {
         const response = await axios.post(
-          "http://full-book-online-store-nqqu-hyax2vqy0.vercel.app/api/v1/add-book",
+          "https://full-book-online-store-nqqu-hyax2vqy0.vercel.app/api/v1/add-book",
           Data,
           { headers }
         );
