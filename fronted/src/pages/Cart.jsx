285f0ffd16c894143d0497675a3d8d0bfd4e50c1
Hafsa Saeed <hafsasaeed1074@gmail.com>
2026-09-21 09:26:44 -0700

    Update API URL for fetching user cart

--- fronted/src/pages/Cart.jsx
+++ fronted/src/pages/Cart.jsx
@@ -17,7 +17,7 @@ const Cart = () => {
   // 🟢 Fetch user cart
   useEffect(() => {
     axios
-      .get("http://full-book-online-store-nqqu-hyax2vqy0.vercel.app/api/v1/get-user-cart", { headers })
+      .get("https://full-book-online-store-nqqu-hyax2vqy0.vercel.app/api/v1/get-user-cart", { headers })
       .then((res) => {
         setCart(res.data.data);
       })
