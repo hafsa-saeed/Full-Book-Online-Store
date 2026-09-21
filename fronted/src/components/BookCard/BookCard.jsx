10028f765bbbd224a2aa6033e45884d555d87582
Hafsa Saeed <hafsasaeed1074@gmail.com>
2026-09-21 08:43:07 -0700

    Update API endpoint for removing favorite book

--- fronted/src/components/BookCard/BookCard.jsx
+++ fronted/src/components/BookCard/BookCard.jsx
@@ -10,7 +10,7 @@ const BookCard = ({ data, favourite }) => {
   };
   const handleRemoveBook = async () => {
     const response = await axios.put(
-      "http://localhost:1000/api/v1/remove-book-from-favourite",
+      "https://full-book-online-store-nqqu-hyax2vqy0.vercel.app/api/v1/remove-book-from-favourite",
       {},
       { headers }
     );
