0c159ba4c191c3b3e6cb165e138c2da156684344
Hafsa Saeed <hafsasaeed1074@gmail.com>
2026-09-21 20:52:10 -0700

    Refactor CORS middleware usage
    Simplified CORS configuration to use default settings.

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,15 @@ const cors = require("cors");
 
 app.use(express.json());
 
-app.use(
-  cors({
+ /* app.use(
+ cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
-);
+);*/
+
+app.use(cors());
 
 require("dotenv").config();
 require("./conn/conn");
